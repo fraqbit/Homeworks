de20330064babcc8c34425fd9c0a17730a71155b
fraqbit <51048342+fraqbit@users.noreply.github.com>
2020-05-23 01:07:26 +0600

    Add files via upload

---
+++ Assembler(4).cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <locale.h>
+#include <cstdlib>
+
+int main()
+{
+	std::string s;
+	std::string buff;
+    int z;
+	_asm {
+        push ebx   
+        push ecx
+        push ebx   
+        lea eax, s  
+        push eax
+        call scanf 
+        add esp, 8  
+        pop ecx    
+        pop ebx
+        mov ax, data
+        mov ds, ax
+        mov es, ax
+
+        push ebx
+        push ecx
+        push ebx
+        lea eax, z
+        push eax
+        call scanf
+        add esp, 8
+        pop ecx
+        pop ebx
+        mov ax, data
+        mov ds, ax
+        mov es, ax
+
+        lea di, buff
+        mov cx, z
+        L1:
+            push    cx
+            lea si, s
+            mov cx, len
+            rep movsb
+            pop cx
+            loop L1
+
+            mov ax, 0900h
+            lea dx, buff
+            int 21h
+
+            mov ax, 0100h
+            int 21h
+	}
+}
